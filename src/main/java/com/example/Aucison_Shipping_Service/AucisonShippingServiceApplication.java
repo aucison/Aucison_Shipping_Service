536ec417ad400b26dbe4272ec83c9e4f9f6f0a43
seungmio <68382824+seungmio@users.noreply.github.com>
2023-08-14 02:13:54 +0900

    feat: Project start

---
+++ src/main/java/com/example/Aucison_Shipping_Service/AucisonShippingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Aucison_Shipping_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AucisonShippingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AucisonShippingServiceApplication.class, args);
+	}
+
+}
