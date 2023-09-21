c2e0630abb8404828d6dd5aa604b68b4e31579ba
seungmio <68382824+seungmio@users.noreply.github.com>
2023-09-21 15:53:40 +0900

    feat: add PaymentsRepository class

---
+++ src/main/java/com/example/Aucison_Shipping_Service/jpa/PaymentsRepository.java
@@ -0,0 +1,6 @@
+package com.example.Aucison_Shipping_Service.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentsRepository extends JpaRepository<Payments, Long> {
+}
